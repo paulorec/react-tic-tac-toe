334d103bdb51da2b0c7b90ca24086dc641f2c21f
Paulo Amorim <paulorec@gmail.com>
2017-08-07 22:23:07 -0300

    :tada: first commit

--- src/index.js
+++ src/index.js
@@ -20,21 +20,76 @@ class Board extends React.Component {
       xIsNext : true
     };
 }
+reset() {
+  this.setState({squares : Array(9).fill(null)});
+}
+
 handleClick(i) {
   const squares = this.state.squares.slice();
-  squares[i] = this.state.xIsNext ? 'X' : 'O';
-  this.setState({squares : squares, xIsNext : !this.state.xIsNext});
+
+  if(squares[i] == null) {
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({squares : squares, xIsNext : !this.state.xIsNext});
+  }
+
 } 
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
 
-  render() {
-    const status = 'Next player: X';
+ calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+   }
+   return null;
+  }
 
+  hasEmptySquare(squares) {
+    for(let i =0;i<squares.length;i++) {
+      if(squares[i] === null)
+        return true;
+    }
+
+    return false;
+  }
+
+  render() {
+    
+    const hasEmptySquare = this.hasEmptySquare(this.state.squares),
+           winner = this.calculateWinner(this.state.squares),
+           isOver = hasEmptySquare || winner !== null;
+    
+    let status;
+
+    if(!hasEmptySquare) {
+      status = `Game is over.  Noboy won`
+    } else if(winner != null) {
+        status = `Game is over.  Player ${(this.state.xIsNext ? 'O' : 'X')} won`
+    } else {
+        status = 'next player is ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    
+    if(isOver) {
+      status = status + " "
+    }
+    
     return (
       <div>
         <div className="status">{status}</div>
+        
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -50,6 +105,7 @@ handleClick(i) {
           {this.renderSquare(7)}
           {this.renderSquare(8)}
         </div>
+        <button name='playAgain' onClick={() => this.reset()}>Start Over</button>
       </div>
     );
   }
